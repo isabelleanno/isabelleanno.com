0e826549d1017867fb4cb526f71e636817833e5a
isabelleanno <itsisabelleanno@gmail.com>
2023-11-24 16:36:23 +0100

    Improved the project timeline

--- src/sect5.js
+++ src/sect5.js
@@ -1,29 +1,52 @@
 import React from "react";
 import Sect5Layout from "./sect5Layout";
+/*
+This section is unique from all the other sections. It is responsible for displaying the projects section and slides.
+My goal is to easily be able to add a project to the code, without having to reformat everything in the timeline,
+and without having to just copy and paste JSX over and over like a noob. I still have a lot to learn, 
+but I decided this would be a good start- so bear with me. 
 
-export default function Sect5(props) {
+In this document, I'm creating a simulated database of projects under the variable name "projects."
+I'm passing them over to the function createProject, which formats the data into a uniform object, like a template.
+These all get passed as props to sect5Layout.js to be formatted into the timeline. (go there if you're confused.)
+*/
+export default function Sect5() {
+  //This function creates a new project using this object as a template.
+  function createProject(title, description, photoSrc, date) {
+    return {
+      Title: title,
+      Description: description,
+      PhotoSrc: photoSrc,
+      Date: date,
+    };
+  }
+  /*An array of values for all my projects, sent to createProject function to reformat into an object.
+Add a new project here, as the first call to createProject().*/
   var projects = [
-    {
-      Title: "This is my project",
-      Description: "Test description",
-      PhotoSrc: "./Images/placeholder.png",
-      Date: "23 September, 2021",
-    },
-    {
-      Title: "This is my other project",
-      Description: "Test description 2",
-      PhotoSrc: "Test Photo",
-      Date: "23 September, 2021",
-    },
-    {
-      Title: "This is my first ever project",
-      Description: "Test description 3",
-      PhotoSrc: "Test Photo",
-      Date: "20 November, 2020",
-    },
+    //Newest project below vv Add a new project right below this line to keep it in order.
+    createProject(
+      "This is my project",
+      "Test description",
+      "./Images/placeholder.png",
+      "23 September, 2021"
+    ),
+    createProject(
+      "This is my other project",
+      "Test description 2",
+      "Test Photo",
+      "23 September, 2021"
+    ),
+    //Oldest project below
+    createProject(
+      "This is my first-ever project",
+      "Test description 3",
+      "Test Photo",
+      "20 November, 2020"
+    ),
   ];
+
+  //Returns the first two slides and then calls sect5Layout component to render the projects in the timeline
   return (
-    /*----------------------------------- Start of Section intro vvv ----------------*/
     <div className="section hs5">
       <div className="slide">
         <span className="title">Projects</span>
--- src/sect5Layout.js
+++ src/sect5Layout.js
@@ -1,107 +1,81 @@
-import React, { Component } from "react";
+import React from "react";
+import ProjTimeline from "./projTimeline";
+/*This file maps and displays the image, title, and description of each project. 
+
+It will display this differently depending on if the index is an even or odd number.  (I just flip the col-6's around
+every other slide to keep things interesting.)
+
+Passes the date, the index, and the lastSlide var to ProjTimeline.js to determine how to display the red timeline
+visual with date.
+*/
 
 export default function Sect5Layout(props) {
   var slides = props.slides;
   var lastSlide = props.slides.length - 1;
+
   return (
     <div>
+      {/*Map the props passed from sect5.js */}
       {slides.map(function (slide, index) {
-        if (index == 0) {
-          return (
-            <div key={index}>
-              <div className="slide">
-                <div className="container">
-                  <div className="row">
-                    <div className="col-12 d-flex">
-                      <div className="col-6 d-flex flex-column align-items-center justify-content-center">
-                        <img src={require("./Images/placeholder.png")} alt="" />
-                      </div>
-                      <div className="col-6 d-flex flex-column align-items-center justify-content-center mx-3">
-                        <h1 className="text-center IBM">{slide.Title}</h1>
-                        <p>{slide.Description}</p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-
-                <div className="col-12 mt-5 d-flex flex-column align-items-end">
-                  <hr className="timelineHR" id="firstHR" />
-                  <div className="container">
-                    <div className="row">
-                      <div className="col-6 d-flex flex-column align-items-center justify-content-center">
-                        <a className="sidenav-btn btn-horizontal"></a>
-                        <p className="date m-0">{slide.Date}</p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          );
-        } else if (index == lastSlide) {
-          return (
-            <div key={index}>
-              <div className="slide">
-                <div className="container">
-                  <div className="row">
-                    <div className="col-12 d-flex">
-                      <div className="col-6 d-flex flex-column align-items-center justify-content-center">
-                        <img src={require("./Images/placeholder.png")} alt="" />
-                      </div>
-                      <div className="col-6 d-flex flex-column align-items-center justify-content-center mx-3">
-                        <h1 className="text-center IBM">{slide.Title}</h1>
-                        <p>{slide.Description}</p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-
-                <div className="col-12 mt-5 d-flex flex-column align-items-start">
-                  <hr className="timelineHR" id="timelineHREnd" />
-                  <div className="container">
-                    <div className="row">
-                      <div className="col-6 d-flex flex-column align-items-center justify-content-center">
-                        <a className="sidenav-btn btn-horizontal"></a>
-                        <p className="date m-0">{slide.Date}</p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          );
-        } else {
-          return (
-            <div key={index}>
-              <div className="slide">
-                <div className="container">
-                  <div className="row">
-                    <div className="col-12 d-flex">
-                      <div className="col-6 d-flex flex-column align-items-center justify-content-center mx-3">
-                        <h1 className="text-center IBM">{slide.Title}</h1>
-                        <p>{slide.Description}</p>
-                      </div>
-                      <div className="col-6 d-flex flex-column align-items-center justify-content-center">
-                        <img src={require("./Images/placeholder.png")} alt="" />
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div className="col-12 mt-5 d-flex flex-column align-items-end">
-                  <hr className="timelineHR" />
-                  <div className="container">
-                    <div className="row">
-                      <div className="col-6 d-flex flex-column align-items-center justify-content-center">
-                        <a className="sidenav-btn btn-horizontal"></a>
-                        <p className="date m-0">{slide.Date}</p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          );
+        //check if the index is even
+        function isEven(index) {
+          return index % 2 === 0;
         }
+        {
+          /*Return generic outer wrapping of each slide with unique key */
+        }
+        return (
+          <div key={index}>
+            <div className="slide">
+              <div className="container">
+                <div className="row">
+                  {(() => {
+                    //If the index is even, render it so the image is on the left side
+                    if (isEven(index) === true) {
+                      return (
+                        <div className="col-12 d-flex">
+                          <div className="col-6 d-flex flex-column align-items-center justify-content-center">
+                            <img
+                              src={require("./Images/placeholder.png")}
+                              alt=""
+                            />
+                          </div>
+                          <div className="col-6 d-flex flex-column align-items-center justify-content-center mx-3">
+                            <h1 className="text-center IBM">{slide.Title}</h1>
+                            <p>{slide.Description}</p>
+                          </div>
+                        </div>
+                      );
+                    } else {
+                      //If the index is odd, render it so the image is on the right side
+                      return (
+                        <div className="col-12 d-flex">
+                          <div className="col-6 d-flex flex-column align-items-center justify-content-center mx-3">
+                            <h1 className="text-center IBM">{slide.Title}</h1>
+                            <p>{slide.Description}</p>
+                          </div>
+                          <div className="col-6 d-flex flex-column align-items-center justify-content-center">
+                            <img
+                              src={require("./Images/placeholder.png")}
+                              alt=""
+                            />
+                          </div>
+                        </div>
+                      );
+                    }
+                  })()}
+                </div>
+              </div>
+              {/* Passing the index, the date and the lastSlide variable to the projTimeline component to display timeline */}
+              <ProjTimeline
+                index={index}
+                Date={slide.Date}
+                lastSlide={lastSlide}
+                isEven={isEven(index)}
+              />
+            </div>
+          </div>
+        );
       })}
     </div>
   );
