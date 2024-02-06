d882d2067869d289fb9b75e7a5758a871358c8f2
Isabelle Anno <isabelleanno.webdev@gmail.com>
2024-02-06 17:31:17 +0100

    Reverted because idk what to do
    I'll figure it out later, i gotta eat dinner

--- src/sect3.jsx
+++ src/sect3.jsx
@@ -1,170 +1,18 @@
-import React, { useEffect } from "react";
-import Sect3Layout from "./sect3Layout";
-import $ from "jquery";
-/*
-This section is unique from all the other sections. It is responsible for displaying the projects section and slides.
-My goal is to easily be able to add a project to the code, without having to reformat everything in the timeline,
-and without having to just copy and paste JSX over and over like a noob. I still have a lot to learn, 
-but I decided this would be a good start- so bear with me. 
+import React from "react";
+import "./css/styles.css";
 
-In this document, I'm creating a simulated database of projects under the variable name "projects."
-I'm passing them over to the function createProject, which formats the data into a uniform object, like a template.
-These all get passed as props to sect5Layout.js to be formatted into the timeline. (go there if you're confused.)
-*/
 export default function Sect3() {
-  //This function creates a new project using this object as a template.
-  function createProject(title, description, photoSrc, link, date) {
-    return {
-      Title: title,
-      Description: description,
-      PhotoSrc: (
-        <img
-          src={`/Images/projects/${photoSrc}.png`}
-          alt="A display of the current project"
-        ></img>
-      ),
-      link: link,
-      Date: date,
-    };
-  }
-  /*An array of values for all my projects, sent to createProject function to reformat into an object.
-Add a new project here, as the first call to createProject().*/
-  var projects = [
-    /*
-    Copy the template below and add it right above this code to add the most recent project.
-    ATTENTION: for photoSrc, it has to be a .png. Only put the actual title (ex. henry.png just becomes henry)
-
-     createProject("title", "Description", "placeholder", "Date Completed"),
-
-    */
-    createProject(
-      "Jokes & Poems AI Generator",
-      "I decided to learn more about AI as a fun side-project. I used the SheCodes AI API to make an interface for an AI joke generator and a poem generator. The user's input is trimmed and run through a profanity filter, then sent to the API using AJAX. I learned about prompt engineering & giving the AI context to generate the best responses.",
-      "jokes",
-      "https://ai-joke-generator.netlify.app/",
-      "December 3, 2023"
-    ),
-    createProject(
-      "Better Balance Safer You",
-      "My old tae kwon do instructor is starting a self-defense program for seniors, and needs my help making a website for it. Eagerly, I deployed my WordPress skills to make him a site. Although it's not hand-coded, I added custom CSS and JavaScript- something non-developers can't offer. Although it's still a work in progress, I'm proud of it.",
-      "BBSU",
-      "https://www.balancesaferu.com/",
-      "November, 2023 - TODAY"
-    ),
-    createProject(
-      "React Dictionary App",
-      "This was my second React project, using two API's: one for the definition, and the other for the images. I enjoyed this project and am proud of it. It cemented the concepts I learned from my first ever React project.",
-      "dictionary",
-      "https://react-dictionary-ia.netlify.app/",
-      "July 7, 2023"
-    ),
-    createProject(
-      "React Weather App",
-      "I finished college and learned React.js from SheCodes, where I built this working weather app in React. It's a significant improvement from the one I made in 2021 and was the first time I used react. I learned about components and the UseState hook. I grew fond of React. It's much more organized and powerful than vanilla JS.",
-      "weather-react",
-      "https://react-weather-ia.netlify.app/",
-      "July 7, 2023"
-    ),
-    createProject(
-      "Mama Lu's",
-      "This was my absolute favorite project in college. I made this site for my PHP professor, Kathy Ison. She is retiring soon and loves baking, so I made the logo in inkscape and coded site from scratch using PHP. It was a group project and a special honor. No other students besides my team got this privelage - I felt so lucky. Professor Ison and I are still in touch, and I am very fond of her.",
-      "mamalu's",
-      "http://mamalus.infinityfreeapp.com/bakery-website/index.php",
-      "May 5, 2023"
-    ),
-    createProject(
-      "The Book Nook",
-      "I polished and improved my skills with this project. Each image was edited to fit the color scheme, and I made the background images myself. It was a fun process and really gave me confidence in my abilities.",
-      "booknook",
-      "https://the-book-nook-ia.netlify.app/",
-      "March 2, 2023"
-    ),
-    createProject(
-      "Web Graphics Design",
-      "This was one of my favorite classes in college. We learned how to use GIMP and InkScape, and the difference between vector images, JPG and PNG. I benefitted tremendously from these projects and still use image editing skills to this day.",
-      "graphicdesign",
-      "https://webgraphicsdesign.netlify.app/",
-      "September-November of 2022"
-    ),
-    createProject(
-      "A Thousand Words Co.",
-      "This site taught me more about using images online. We discussed copyright laws, licenses and fair use. I decided to source all the images from unplash.com, which is fair use.",
-      "athousandwords",
-      "https://a-thousand-words.netlify.app/",
-      "October 26, 2022"
-    ),
-    createProject(
-      "Chairing Is Caring Co.",
-      "I made a fake architecture company using Bootstrap as the final project of my advanced CSS class. It's fully responsive, and I'm proud of the color scheme and fonts used.",
-      "chairing",
-      "https://chairing-is-caring.netlify.app/",
-      "May 24, 2022"
-    ),
-    createProject(
-      "JavaScript Puzzle Final Project",
-      "Although not responsive on mobile devices, I'm proud of this puzzle game as it goes above and beyond and uses the unsplash API- a skill I learned from SheCodes- to find pictures on the web. You can use the settings to choose the difficulty, and every image is unique.",
-      "JSPuzzle",
-      "https://javascript-puzzle.netlify.app/",
-      "May 4, 2022"
-    ),
-    createProject(
-      "Kirby Club",
-      "This site was coded with Bootstrap. It taught me the grid system and other features of the popular framework.",
-      "kirby",
-      "https://kirby-club.netlify.app/",
-      "May 3, 2022"
-    ),
-    createProject(
-      "Early JavaScript Projects",
-      "Here is a collection of simple javascript projects. They were made in my first semester of college. They taught me a lot about vanilla JavaScript. I expanded these skills with JQuery and React.js. In this collection of projects, I made a tip calculator, a quiz and a list of items that the user can add or delete.",
-      "arcade",
-      "https://javascript.isabelleanno.com/",
-      "March-May of 2022"
-    ),
-    createProject(
-      "Green Thumb Succulent Co.",
-      "I created this fake plant store company for my advanced CSS class. I doodled the logo myself using Krita. I'm proud of the layout of the shop page. All in all, it's a good site considering the little experience I had with coding at this point.",
-      "greenthumb",
-      "https://green-thumb-succulents.netlify.app/",
-      "February 3, 2022"
-    ),
-
-    createProject(
-      "Seize the Clay Pottery Studio",
-      "One of my first ever projects in college. It helped me expand my HTML and CSS knowledge.",
-      "pottery",
-      "https://seize-the-clay.netlify.app/",
-      "October 18, 2021"
-    ),
-    createProject(
-      "Weather Website",
-      "This weather site was made during my SheCodes Plus course. It uses an API from openweathermap.com to get real-time weather data. I also learned basic JavaScript and the concept of APIs. I am proud of it, but I still haven't yet learned responsive techniques for making it look good on every device.",
-      "weather",
-      "https://weather-isa.netlify.app/",
-      "May 26, 2021"
-    ),
-    //Oldest project below
-    createProject(
-      "My dog Henry 🐾",
-      "Here is my first official website. I made it while learning HTML and CSS through SheCodes Basic. Henry is my best buddy. He passed away peacefully in his bed at age 15 in November of 2022, and had a great life. He'll always have a special place in my heart. Even though this website is not the best, we all have to start somewhere, and I'm glad to have started my career with him by my side.",
-      "henry",
-      "https://www.shecodes.io/cohorts/shecodes-online-workshop-57-0/projects/284319",
-      "March 22, 2021"
-    ),
-  ];
-  useEffect(() => {
-    $(".fp-arrow.fp-next").addClass("sect5FPNext");
-    $(".fp-arrow.fp-prev").addClass("sect5FPPrev");
-  });
-  //Returns the first two slides and then calls sect5Layout component to render the projects in the timeline
   return (
     <div className="section hs3">
-      <div className="slide">
-        <span className="title">Projects</span>
-        <h1 id="Projects">Projects</h1>
+      <div className="container">
+        <div className="row">
+          <div className="col" id="Degrees">
+            <span className="title">Education Title</span>
+            <h1 id="Education">Degrees</h1>
+            <h1 id="Education">& Certs</h1>
+          </div>
+        </div>
       </div>
-      <span className="title">Projects</span>
-      <Sect3Layout slides={projects} />
     </div>
   );
 }
--- src/sect4.jsx
+++ src/sect4.jsx
@@ -1,14 +1,299 @@
 import React from "react";
-import "./css/styles.css";
-export default function Sect4() {
+export default function Sect5() {
   return (
+    /*---------------------------------------------------- INTRO START vvv---------------------- */
     <div className="section hs4">
       <div className="container">
-        <div className="row">
-          <div className="col" id="Degrees">
-            <span className="title">Education Title</span>
-            <h1 id="Education">Degrees</h1>
-            <h1 id="Education">& Certs</h1>
+        <div className="row my-5">
+          <span className="title">Education Content</span>
+          <div
+            className="col-12 my-5 d-flex flex-column align-items-center frame"
+            id="timeline-frame"
+          >
+            <div className="col-12 d-flex justify-content-center py-5 my-1">
+              <h1 className="display-1 text-center ">
+                A Timeline of my Education
+              </h1>
+            </div>
+            <div
+              className="col-xs-12 col-sm-9 col-md-9 col-lg-9 col-xl-9
+             d-flex flex-column"
+              id="timeline-wrapper"
+            >
+              {/*-------------------------End of INTRO ^^^ | Start of ENTRY 1 vvv ----------------*/}
+              <div className="col-12 d-flex">
+                <div className="col-2 d-flex align-items-center justify-content-end  desktop-small">
+                  <p className="m-0 bold">Feb. 18, 2021</p>
+                </div>
+                <div className="col-1 d-flex justify-content-center align-items-center desktop-small">
+                  <a className="sidenav-btn"></a>
+                  <a className="separator"></a>
+                </div>
+                <div className="col my-5">
+                  <p className=" bold mobile-small">Feb. 18, 2021</p>
+                  <h1 className="IBM bold">Made my first website</h1>
+                  <p>
+                    I was 19, and it was just an experimental index.html. It's
+                    not much, but everyone has to start somewhere. Prior to
+                    this, I was studying dental hygiene at the University of
+                    Cincinnati (UC). This was a big accomplishment in and of
+                    itself because the degree is competitive. Many apply but
+                    only a select few get in based on their grades &
+                    performance. While it was fun learning about biology and
+                    anatomy, I realized the job wasn't for me. I needed
+                    something more creative. On this day, I decided to try web
+                    development and haven't looked back!
+                  </p>
+                </div>
+              </div>
+              {/*-------------------------End of entry 1 ^^^ | Start of entry 2 vvv ----------------*/}
+              <div className="col-12 d-flex">
+                <div className="col-2 d-flex align-items-center justify-content-end  desktop-small">
+                  <p className="m-0 bold">March 22, 2021</p>
+                </div>
+                <div className="col-1 d-flex justify-content-center align-items-center desktop-small">
+                  <a className="sidenav-btn"></a>
+                  <a className="separator"></a>
+                </div>
+                <div className="col my-5">
+                  <p className=" bold mobile-small">March 22, 2021</p>
+                  <h1 className="IBM bold">Graduated SheCodes Basic</h1>
+                  <p>
+                    Wanting to learn more about coding, I embarked on a journey
+                    to find a good starting point. I decided to join SheCodes
+                    because it seemed like a nice community of women helping
+                    each other learn how to code. I took all their courses, and
+                    I'm so glad I did. They taught me a lot of skills including
+                    bootstrap and react.js, as well as give me a head start in
+                    college. Through SheCodes Basic, I made my first real
+                    website about my dog Henry.
+                  </p>
+                  <img
+                    src="/Images/certs/basic.png"
+                    className="ExtraDim"
+                    alt="SheCodes Verified Certificate of Achievement. March 22, 2021. Isabelle Anno has successfully completed the introduction to Coding workshop."
+                  />
+                </div>
+              </div>
+              {/*-------------------------End of entry 2 ^^^ | Start of entry 3 vvv ----------------*/}
+              <div className="col-12 d-flex">
+                <div className="col-2 d-flex align-items-center justify-content-end  desktop-small">
+                  <p className="m-0 bold">May 31, 2021</p>
+                </div>
+                <div className="col-1 d-flex justify-content-center align-items-center  desktop-small">
+                  <a className="sidenav-btn"></a>
+                  <a className="separator"></a>
+                </div>
+                <div className="col my-5">
+                  <p className=" bold mobile-small">May 31, 2021</p>
+                  <h1 className="IBM bold">Graduated SheCodes Plus</h1>
+                  <p>
+                    After gaining confidence & skills from SheCodes Basic, I
+                    moved on to their second course and worked on a weather
+                    website that uses real-time data from an API and displays it
+                    on an interface. While doing this, I was searching for an
+                    college in Ohio that taught web design & development,
+                    preferably online.
+                  </p>
+                  <img
+                    src="/Images/certs/plus.png"
+                    className="dim"
+                    alt="SheCodes Verified Certificate of Achievement. May 31, 2021. Isabelle Anno has successfully completed the web development workshop."
+                  />
+                </div>
+              </div>
+              {/*-------------------------End of entry 3 ^^^ | Start of entry 4 vvv ----------------*/}
+              <div className="col-12 d-flex">
+                <div className="col-2 d-flex align-items-center justify-content-end  desktop-small">
+                  <p className="m-0 bold">July 5, 2021</p>
+                </div>
+                <div className="col-1 d-flex justify-content-center align-items-center  desktop-small">
+                  <a className="sidenav-btn"></a>
+                  <a className="separator"></a>
+                </div>
+                <div className="col my-5">
+                  <p className=" bold mobile-small">July 5, 2021</p>
+                  <h1 className="IBM bold">Graduated SheCodes Responsive</h1>
+                  <p>
+                    This is where I was first introduced to Bootstrap. I made a
+                    portfolio (now outdated) that's responsive and looks good on
+                    all devices. This was very exciting and I felt so
+                    accomplished! I will go on to greatly refine my Bootstrap
+                    skills in college.
+                  </p>
+                  <img
+                    src="/Images/certs/Responsive.png"
+                    className="dim"
+                    alt="SheCodes Verified Certificate of Achievement. July 5, 2021. Isabelle Anno has successfully completed the Responsive Web Development workshop."
+                  />
+                </div>
+              </div>
+              {/*-------------------------End of entry 4 ^^^ | Start of entry 5 vvv ----------------*/}
+              <div className="col-12 d-flex">
+                <div className="col-2 d-flex align-items-center justify-content-end  desktop-small">
+                  <p className="m-0 bold">Aug 29, 2021</p>
+                </div>
+                <div className="col-1 d-flex justify-content-center align-items-center  desktop-small">
+                  <a className="sidenav-btn"></a>
+                  <a className="separator"></a>
+                </div>
+                <div className="col my-5">
+                  <p className=" bold mobile-small">Aug 29, 2021</p>
+                  <h1 className="IBM bold">Started College at Stark State</h1>
+                  <p>
+                    After enjoying a summer vacation, I was ready to start
+                    college again. Since UC didn't have a web development
+                    program, I transferred to Stark State College where I would
+                    get my associates degree. There I made most of my first
+                    portfolio pieces, and learned A LOT about programming and
+                    web development. The degree could be taken online or
+                    in-person, so I decided I'd do it online due to covid-19 and
+                    because I also eventually wanted to move to the Netherlands.
+                  </p>
+                </div>
+              </div>
+              {/*-------------------------End of entry 5 ^^^ | Start of entry 6 vvv ----------------*/}
+              <div className="col-12 d-flex">
+                <div className="col-2 d-flex align-items-center justify-content-end  desktop-small">
+                  <p className="m-0 bold">May 14, 2023</p>
+                </div>
+                <div className="col-1 d-flex justify-content-center align-items-center  desktop-small">
+                  <a className="sidenav-btn"></a>
+                  <a className="separator"></a>
+                </div>
+                <div className="col my-5">
+                  <p className=" bold mobile-small">May 14, 2023</p>
+                  <h1 className="IBM bold">Graduated College</h1>
+                  <p>
+                    Getting my associate's degree was a rewarding experience. I
+                    graduated with a 4.0 GPA and was on the president's list
+                    every semester. My prior college experience helped me
+                    achieve this, as I knew what to expect and how to manage my
+                    time. Regardless, ultimately I was there to learn and become
+                    the best I can be. I hope to expand my educational journey
+                    in the future and try new things. Constantly learning is
+                    something that appeals to me about the tech industry-
+                    there's always change happening and something new to
+                    contribute to.
+                  </p>
+                  <img
+                    src="/Images/certs/degree.jpg"
+                    className="ExtraDim"
+                    alt="Stark State College. North Canton, Ohio. May 14, 2023. Associate's of applied business web design and development."
+                  />
+                </div>
+              </div>
+              {/*-------------------------End of entry 6 ^^^ | Start of entry 7 vvv ----------------*/}
+              <div className="col-12 d-flex">
+                <div className="col-2 d-flex align-items-center justify-content-end  desktop-small">
+                  <p className="m-0 bold">May 14, 2023</p>
+                </div>
+                <div className="col-1 d-flex justify-content-center align-items-center  desktop-small">
+                  <a className="sidenav-btn"></a>
+                  <a className="separator"></a>
+                </div>
+                <div className="col my-5">
+                  <p className=" bold mobile-small">May 14, 2023</p>
+                  <h1 className="IBM bold">Got two certificates</h1>
+                  <p>
+                    Alongside my college degree, I earned two certificates along
+                    the way. One in web design, and another in SQL, specifically
+                    using SSMS.
+                  </p>
+                  <div className="col d-flex ExtraDim" id="certs-border">
+                    <div className="col-6">
+                      <img
+                        src="/Images/certs/sql.jpg"
+                        alt="Stark State College Career Enhancement Certificate in Microsoft SQL Server. May 14, 2023."
+                      />
+                    </div>
+                    <div className="col-6">
+                      <img
+                        src="/Images/certs/webdesign.jpg"
+                        alt="Stark State College Career Enhancement Certificate in Web Design. May 14, 2023."
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+              {/*-------------------------End of entry 7 ^^^ | Start of entry 8 vvv ----------------*/}
+              <div className="col-12 d-flex">
+                <div className="col-2 d-flex align-items-center justify-content-end  desktop-small">
+                  <p className="m-0 bold">July 19, 2023</p>
+                </div>
+                <div className="col-1 d-flex justify-content-center align-items-center  desktop-small">
+                  <a className="sidenav-btn"></a>
+                  <a className="separator"></a>
+                </div>
+                <div className="col my-5">
+                  <p className=" bold mobile-small">July 19, 2023</p>
+                  <h1 className="IBM bold">Graduated SheCodes React</h1>
+                  <p>
+                    College takes up a lot of time, so after graduating, I
+                    finished the final SheCodes course, where I learned react.js
+                    and built another weather interface using react, as well as
+                    a working dictionary.
+                  </p>
+                  <img
+                    src="/Images/certs/React.png"
+                    className="dim"
+                    alt="SheCodes Verified Certificate of Achievement. July 19, 2023. Isabelle Anno has successfully completed the Advanced React Development workshop."
+                  />
+                </div>
+              </div>
+              {/*-------------------------End of entry 8 ^^^ | Start of entry 9 vvv ----------------*/}
+              <div className="col-12 d-flex">
+                <div className="col-2 d-flex align-items-center justify-content-end  desktop-small">
+                  <p className="m-0 bold">December 4, 2023</p>
+                </div>
+                <div className="col-1 d-flex justify-content-center align-items-center  desktop-small">
+                  <a className="sidenav-btn"></a>
+                  <a className="separator"></a>
+                </div>
+                <div className="col my-5">
+                  <p className=" bold mobile-small">December 4, 2023</p>
+                  <h1 className="IBM bold">SheCodes AI Addon Certification</h1>
+                  <p>
+                    With this certificate, I learned more about AI technology
+                    and prompt engineering. It's certainly an intruguing topic
+                    for me. Since AI is changing so rapidly, it was hard to find
+                    a place to start learning about it. This course helped me
+                    realize that, when used correctly, AI can be a great tool
+                    for a variety of purposes, and to not be afraid of it.
+                  </p>
+                  <img
+                    src="/Images/certs/AI.png"
+                    className="dim"
+                    alt="SheCodes Verified Certificate of Achievement. December 4, 2023. Isabelle Anno has successfully completed the Introduction to Artificial Intelligence workshop."
+                  />
+                </div>
+              </div>
+              {/*-------------------------End of Last Entry ^^^ | Start of PRESENT vvv ----------------*/}
+              <div className="col-12 d-flex">
+                <div className="col-2 d-flex align-items-center justify-content-end  desktop-small">
+                  <p className="m-0 bold">Today</p>
+                </div>
+                <div className="col-1 d-flex justify-content-center align-items-center  desktop-small">
+                  <a className="sidenav-btn"></a>
+                  <a className="separator"></a>
+                </div>
+                <div className="col my-5">
+                  <p className=" bold mobile-small">Today</p>
+                  <h1 className="IBM bold">Started my Career</h1>
+                  <a
+                    className="btn-link-reverse mt-3 ms-1"
+                    href="IsabelleAnno-Resume.pdf"
+                    download="IsabelleAnno-Resume.pdf"
+                  >
+                    Download my Resume
+                  </a>
+                  <a className="btn-link-reverse mt-3 ms-1" href="#s5">
+                    See My Projects
+                  </a>
+                </div>
+              </div>
+              {/*-------------------------End of PRESENT ^^^ | END OF COMPONENT vvv ----------------*/}
+            </div>
           </div>
         </div>
       </div>
