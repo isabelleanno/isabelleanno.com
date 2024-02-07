df0ddbbab87404165c25edc2cfe8a04879f56249
Isabelle Anno <isabelleanno.webdev@gmail.com>
2024-02-07 12:16:40 +0100

    Fixed age.js
    It said I was 23 when I am still 22 and that bothers me because I don't want to feel older than I am. This fix will me from some extra depression in a few decades.

--- src/age.jsx
+++ src/age.jsx
@@ -2,8 +2,21 @@
 
 import React from "react";
 export default function Age() {
-  let today = new Date();
-  let myBirthday = new Date("2001-04-23");
-  let myAge = today.getFullYear() - myBirthday.getFullYear();
-  return <span>{myAge}</span>;
+  //Define the current date, day, and month.
+  let date = new Date();
+  let day = date.getDate();
+  let month = date.getMonth() + 1;
+
+  //Define my birthday and current age variable
+  const myBirthday = new Date("2001-04-23");
+  let myCurrentAge = date.getFullYear() - myBirthday.getFullYear();
+
+  //This statement is necessary because I noticed it adds a year every January 1- before my birthday.
+  //So I said, if it's before my birthday, subtract a year from the age. You can test it by adding any date above in the
+  //"date" variable. ^^^ ;)
+  if (day < 23 && month < 4) {
+    myCurrentAge -= 1;
+  }
+
+  return <span>{myCurrentAge}</span>;
 }
